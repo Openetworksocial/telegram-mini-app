b992c92aea22debc410601e4596a546779958a14
tanmay2626 <tanmaywaykar17@gmail.com>
2024-07-23 03:08:44 +0530

    feat: update room route

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout";
 import Finish from "./pages/Finish";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Finish />} />
+          <Route path="/" element={<Layout />} />
         </Routes>
       </Router>
     </>
